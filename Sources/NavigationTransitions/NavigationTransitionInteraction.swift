1ca94f65559adcf957c38bdddb280ba18d164732
Ydna <0xEFEFEF@gmail.com>
2024-12-18 22:39:56 +0800

    refactor: finetune animation

--- Sources/NavigationTransitions/NavigationTransitionInteraction.swift
+++ Sources/NavigationTransitions/NavigationTransitionInteraction.swift
@@ -9,44 +9,43 @@ extension UINavigationController {
 			delegate.interactionController = nil
 			return
 		}
-
+		
 		let translation = gestureRecognizer.translation(in: gestureRecognizerView).x
 		let width = gestureRecognizerView.bounds.size.width
 		let percent = translation / width
-
+		
 		switch gestureRecognizer.state {
 		case .possible:
 			break
-
+			
 		case .began:
 			delegate.interactionController = UIPercentDrivenInteractiveTransition()
 			popViewController(animated: true)
 			delegate.interactionController?.update(percent)
-
+			
 		case .changed:
 			delegate.interactionController?.update(percent)
-
+			
 		case .ended:
 			let velocity = gestureRecognizer.velocity(in: gestureRecognizerView).x
-
-			if velocity > 675 || (percent >= 0.2 && velocity > -200) {
-				let resistance: Double = 800
-				let maxSpeed: Double = 2.25
-				let nominalSpeed = max(0.99, velocity / resistance)
-				let speed = min(nominalSpeed, maxSpeed)
-
+			let resistance: Double = 800
+			let maxSpeed: Double = 1.2
+			let nominalSpeed = max(0.7, velocity / resistance)
+			let speed = min(nominalSpeed, maxSpeed) * 0.6
+			if velocity > 675 || (percent >= 0.5 && velocity > -200) {
 				delegate.interactionController?.completionSpeed = speed
 				delegate.interactionController?.finish()
 			} else {
+				delegate.interactionController?.completionSpeed = speed
 				delegate.interactionController?.cancel()
 			}
-
+			
 			delegate.interactionController = nil
-
+			
 		case .failed, .cancelled:
 			delegate.interactionController?.cancel()
 			delegate.interactionController = nil
-
+			
 		@unknown default:
 			break
 		}
